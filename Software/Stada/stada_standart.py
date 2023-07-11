138e5bb8fc83447a8322e20f033ecf54b6b8f893
Van Gulinyan <vangulinyan@gmail.com>
2023-07-11 15:24:50 +0300

    Stada test file
    Added standart test function for standa controls

---
+++ Software/Stada/stada_standart.py
@@ -0,0 +1,262 @@
+# Connecting libs
+from ctypes import *
+import time
+import os
+import sys
+import platform
+import tempfile
+import re
+
+if sys.version_info >= (3,0):
+    import urllib.parse
+
+# Dependences
+    
+# For correct usage of the library libximc,
+# you need to add the file pyximc.py wrapper with the structures of the library to python path. Other way it won't work.
+cur_dir = os.path.abspath(os.path.dirname(__file__)) # Specifies the current directory.
+ximc_dir = os.path.join(cur_dir, "..", "..", "..", "ximc") # Formation of the directory name with all dependencies. The dependencies for the examples are located in the ximc directory.
+ximc_package_dir = os.path.join(ximc_dir, "crossplatform", "wrappers", "python") # Formation of the directory name with python dependencies.
+sys.path.append(ximc_package_dir)  # add pyximc.py wrapper to python path
+
+# Depending on your version of Windows, add the path to the required DLLs to the environment variable
+# bindy.dll
+# libximc.dll
+# xiwrapper.dll
+if platform.system() == "Windows":
+    # Determining the directory with dependencies for Windows depending on the bit depth.
+    arch_dir = "win64" if "64" in platform.architecture()[0] else "win32" # 
+    libdir = os.path.join(ximc_dir, arch_dir)
+    if sys.version_info >= (3,8):
+        os.add_dll_directory(libdir)
+    else:
+        os.environ["Path"] = libdir + ";" + os.environ["Path"] # add dll path into an environment variable
+
+# check for errors
+try: 
+    from pyximc import * # importing libriry cover (written for c-typed languages, cover must be used for pyhton)
+except ImportError as err:
+    print ("Can't import pyximc module. The most probable reason is that you changed the relative location of the test_Python.py and pyximc.py files. See developers' documentation for details.")
+    exit()
+except OSError as err:
+    # print(err.errno, err.filename, err.strerror, err.winerror) # Allows you to display detailed information by mistake.
+    if platform.system() == "Windows":
+        if err.winerror == 193:   # The bit depth of one of the libraries bindy.dll, libximc.dll, xiwrapper.dll does not correspond to the operating system bit.
+            print("Err: The bit depth of one of the libraries bindy.dll, libximc.dll, xiwrapper.dll does not correspond to the operating system bit.")
+            # print(err)
+        elif err.winerror == 126: # One of the library bindy.dll, libximc.dll, xiwrapper.dll files is missing.
+            print("Err: One of the library bindy.dll, libximc.dll, xiwrapper.dll is missing.")
+            print("It is also possible that one of the system libraries is missing. This problem is solved by installing the vcredist package from the ximc\\winXX folder.")
+            # print(err)
+        else:           # Other errors the value of which can be viewed in the code.
+            print(err)
+        print("Warning: If you are using the example as the basis for your module, make sure that the dependencies installed in the dependencies section of the example match your directory structure.")
+        print("For correct work with the library you need: pyximc.py, bindy.dll, libximc.dll, xiwrapper.dll")
+    else:
+        print(err)
+        print ("Can't load libximc library. Please add all shared libraries to the appropriate places. It is decribed in detail in developers' documentation. On Linux make sure you installed libximc-dev package.\nmake sure that the architecture of the system and the interpreter is the same")
+    exit()
+
+# get device info
+def test_info(lib, device_id):
+    print("\nGet device info")
+    x_device_information = device_information_t()
+    result = lib.get_device_information(device_id, byref(x_device_information))
+    print("Result: " + repr(result))
+    if result == Result.Ok:
+        print("Device information:")
+        print(" Manufacturer: " +
+                repr(string_at(x_device_information.Manufacturer).decode()))
+        print(" ManufacturerId: " +
+                repr(string_at(x_device_information.ManufacturerId).decode()))
+        print(" ProductDescription: " +
+                repr(string_at(x_device_information.ProductDescription).decode()))
+        print(" Major: " + repr(x_device_information.Major))
+        print(" Minor: " + repr(x_device_information.Minor))
+        print(" Release: " + repr(x_device_information.Release))
+
+# get device status
+def test_status(lib, device_id):
+    print("\nGet status")
+    x_status = status_t()
+    result = lib.get_status(device_id, byref(x_status))
+    print("Result: " + repr(result))
+    if result == Result.Ok:
+        print("Status.Ipwr: " + repr(x_status.Ipwr))
+        print("Status.Upwr: " + repr(x_status.Upwr))
+        print("Status.Iusb: " + repr(x_status.Iusb))
+        print("Status.Flags: " + repr(hex(x_status.Flags)))
+
+# get coordinates
+def test_get_position(lib, device_id):
+    print("\nRead position")
+    x_pos = get_position_t()
+    result = lib.get_position(device_id, byref(x_pos))
+    print("Result: " + repr(result))
+    if result == Result.Ok:
+        print("Position: {0} steps, {1} microsteps".format(x_pos.Position, x_pos.uPosition))
+    return x_pos.Position, x_pos.uPosition
+
+# moving left
+def test_left(lib, device_id):
+    print("\nMoving left")
+    result = lib.command_left(device_id)
+    print("Result: " + repr(result))
+  
+# move by distance
+def test_move(lib, device_id, distance, udistance):
+    print("\nGoing to {0} steps, {1} microsteps".format(distance, udistance))
+    result = lib.command_move(device_id, distance, udistance)
+    print("Result: " + repr(result))
+
+# 
+def test_wait_for_stop(lib, device_id, interval):
+    print("\nWaiting for stop")
+    result = lib.command_wait_for_stop(device_id, interval)
+    print("Result: " + repr(result))
+
+# check for device serial id
+def test_serial(lib, device_id):
+    print("\nReading serial")
+    x_serial = c_uint()
+    result = lib.get_serial_number(device_id, byref(x_serial))
+    if result == Result.Ok:
+        print("Serial: " + repr(x_serial.value))
+
+# get movement speed
+def test_get_speed(lib, device_id)        :
+    print("\nGet speed")
+    # Create move settings structure
+    mvst = move_settings_t()
+    # Get current move settings from controller
+    result = lib.get_move_settings(device_id, byref(mvst))
+    # Print command return status. It will be 0 if all is OK
+    print("Read command result: " + repr(result))    
+    
+    return mvst.Speed
+
+# set movement speed
+def test_set_speed(lib, device_id, speed):
+    print("\nSet speed")
+    # Create move settings structure
+    mvst = move_settings_t()
+    # Get current move settings from controller
+    result = lib.get_move_settings(device_id, byref(mvst))
+    # Print command return status. It will be 0 if all is OK
+    print("Read command result: " + repr(result))
+    print("The speed was equal to {0}. We will change it to {1}".format(mvst.Speed, speed))
+    # Change current speed
+    mvst.Speed = int(speed)
+    # Write new move settings to controller
+    result = lib.set_move_settings(device_id, byref(mvst))
+    # Print command return status. It will be 0 if all is OK
+    print("Write command result: " + repr(result))    
+    
+
+# set microstep mode for accurate positioning
+def test_set_microstep_mode_256(lib, device_id):
+    print("\nSet microstep mode to 256")
+    # Create engine settings structure
+    eng = engine_settings_t()
+    # Get current engine settings from controller
+    result = lib.get_engine_settings(device_id, byref(eng))
+    # Print command return status. It will be 0 if all is OK
+    print("Read command result: " + repr(result))
+    # Change MicrostepMode parameter to MICROSTEP_MODE_FRAC_256
+    # (use MICROSTEP_MODE_FRAC_128, MICROSTEP_MODE_FRAC_64 ... for other microstep modes)
+    eng.MicrostepMode = MicrostepMode.MICROSTEP_MODE_FRAC_256
+    # Write new engine settings to controller
+    result = lib.set_engine_settings(device_id, byref(eng))
+    # Print command return status. It will be 0 if all is OK
+    print("Write command result: " + repr(result))    
+
+
+# variable 'lib' points to a loaded library
+# note that ximc uses stdcall on win
+print("Library loaded")
+
+# creating 64-bit buffer for lib
+sbuf = create_string_buffer(64)
+lib.ximc_version(sbuf)
+print("Library version: " + sbuf.raw.decode().rstrip("\0"))
+
+# This is device search and enumeration with probing. It gives more information about devices. Provides parallel connection up to 32 axes at the same time.
+probe_flags = EnumerateFlags.ENUMERATE_PROBE + EnumerateFlags.ENUMERATE_NETWORK
+enum_hints = b"addr="
+# enum_hints = b"addr=" # Use this hint string for broadcast enumerate
+devenum = lib.enumerate_devices(probe_flags, enum_hints)
+print("Device enum handle: " + repr(devenum))
+print("Device enum handle type: " + repr(type(devenum)))
+
+dev_count = lib.get_device_count(devenum)
+print("Device count: " + repr(dev_count))
+
+controller_name = controller_name_t() 
+for dev_ind in range(0, dev_count):
+    enum_name = lib.get_device_name(devenum, dev_ind)
+    result = lib.get_enumerate_device_controller_name(devenum, dev_ind, byref(controller_name))
+    if result == Result.Ok:
+        print("Enumerated device #{} name (port name): ".format(dev_ind) + repr(enum_name) + ". Friendly name: " + repr(controller_name.ControllerName) + ".")
+
+flag_virtual = 0
+
+# connection and libs choosed for each device
+open_name = None
+if len(sys.argv) > 1:
+    open_name = sys.argv[1]
+elif dev_count > 0:
+    open_name = lib.get_device_name(devenum, 0)
+elif sys.version_info >= (3,0):
+    # use URI for virtual device when there is new urllib python3 API
+    tempdir = tempfile.gettempdir() + "/testdevice.bin"
+    if os.altsep:
+        tempdir = tempdir.replace(os.sep, os.altsep)
+    # urlparse build wrong path if scheme is not file
+    uri = urllib.parse.urlunparse(urllib.parse.ParseResult(scheme="file", \
+            netloc=None, path=tempdir, params=None, query=None, fragment=None))
+    open_name = re.sub(r'^file', 'xi-emu', uri).encode()
+    flag_virtual = 1
+    print("The real controller is not found or busy with another app.")
+    print("The virtual controller is opened to check the operation of the library.")
+    print("If you want to open a real controller, connect it or close the application that uses it.")
+
+if not open_name:
+    exit(1)
+
+if type(open_name) is str:
+    open_name = open_name.encode()
+
+print("\nOpen device " + repr(open_name))
+device_id = lib.open_device(open_name)
+print("Device id: " + repr(device_id))
+
+# test function
+
+test_info(lib, device_id)
+test_status(lib, device_id)
+test_set_microstep_mode_256(lib, device_id)
+startpos, ustartpos = test_get_position(lib, device_id)
+# first move
+test_left(lib, device_id)
+time.sleep(3)
+test_get_position(lib, device_id)
+# second move
+current_speed = test_get_speed(lib, device_id)
+test_set_speed(lib, device_id, current_speed / 2)
+test_move(lib, device_id, startpos, ustartpos)
+test_wait_for_stop(lib, device_id, 100)
+test_status(lib, device_id)
+test_serial(lib, device_id)
+
+print("\nClosing")
+
+# The device_t device parameter in this function is a C pointer, unlike most library functions that use this parameter
+lib.close_device(byref(cast(device_id, POINTER(c_int))))
+print("Done")
+
+# Check for queue 
+if flag_virtual == 1:
+    print(" ")
+    print("The real controller is not found or busy with another app.")
+    print("The virtual controller is opened to check the operation of the library.")
+    print("If you want to open a real controller, connect it or clos
